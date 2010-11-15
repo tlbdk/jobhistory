f49b8a45f35d16ce09c431d60c748279fb01b460
Troels Liebe Bentsen <tlb@rapanden.dk>
2010-11-15 02:14:54 +0100

    Do simple autocomplete

---
+++ frontend/contrib/jobs.js
@@ -0,0 +1,14 @@
+[
+    {
+        "title":"PHP Programmer",
+        "company": "IT-Kartellet",
+    },
+    {
+        "title":"Linux Consultant",
+        "company": "Xenux",
+    },
+    {
+        "title":"System Administrator",
+        "company": "Catpipe Systems ApS",
+    }
+]
---
+++ frontend/contrib/tags.js
@@ -0,0 +1,20 @@
+[
+    {
+        "tag":"PHP",
+        "parent": "Programming",
+        "value":"PHP",
+        "label":"PHP"
+    },
+    {
+        "tag":"Perl",
+        "parent": "Programming",
+        "value":"Perl",
+        "label":"Perl"
+    },
+    {
+        "tag":"C#",
+        "parent": "Programming",
+        "value":"C#",
+        "label":"C#"
+    }
+]
